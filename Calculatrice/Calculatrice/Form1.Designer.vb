ad08af19a2088cb7ba4d99c464148f10fce2d6f4
ralf789 <51427526+ralf789@users.noreply.github.com>
2019-10-30 13:38:45 -0400

    Add files via upload

---
+++ Calculatrice/Calculatrice/Form1.Designer.vb
@@ -0,0 +1,275 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmdChiffre7 = New System.Windows.Forms.Button()
+        Me.cmdChiffre4 = New System.Windows.Forms.Button()
+        Me.cmdChiffre1 = New System.Windows.Forms.Button()
+        Me.cmdEffacer = New System.Windows.Forms.Button()
+        Me.cmdChiffre8 = New System.Windows.Forms.Button()
+        Me.cmdChiffre5 = New System.Windows.Forms.Button()
+        Me.cmdChiffre2 = New System.Windows.Forms.Button()
+        Me.cmdChiffre0 = New System.Windows.Forms.Button()
+        Me.cmdChiffre9 = New System.Windows.Forms.Button()
+        Me.cmdChiffre6 = New System.Windows.Forms.Button()
+        Me.cmdChiffre3 = New System.Windows.Forms.Button()
+        Me.cmdDecimal = New System.Windows.Forms.Button()
+        Me.cmdDiviser = New System.Windows.Forms.Button()
+        Me.cmdPlus = New System.Windows.Forms.Button()
+        Me.cmdMultiplier = New System.Windows.Forms.Button()
+        Me.cmdMoins = New System.Windows.Forms.Button()
+        Me.cmdEgal = New System.Windows.Forms.Button()
+        Me.lblAffichage = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'cmdChiffre7
+        '
+        Me.cmdChiffre7.BackColor = System.Drawing.Color.Gold
+        Me.cmdChiffre7.Location = New System.Drawing.Point(42, 147)
+        Me.cmdChiffre7.Name = "cmdChiffre7"
+        Me.cmdChiffre7.Size = New System.Drawing.Size(31, 27)
+        Me.cmdChiffre7.TabIndex = 7
+        Me.cmdChiffre7.Text = "7"
+        Me.cmdChiffre7.UseVisualStyleBackColor = False
+        '
+        'cmdChiffre4
+        '
+        Me.cmdChiffre4.BackColor = System.Drawing.Color.Gold
+        Me.cmdChiffre4.Location = New System.Drawing.Point(42, 197)
+        Me.cmdChiffre4.Name = "cmdChiffre4"
+        Me.cmdChiffre4.Size = New System.Drawing.Size(31, 27)
+        Me.cmdChiffre4.TabIndex = 4
+        Me.cmdChiffre4.Text = "4"
+        Me.cmdChiffre4.UseVisualStyleBackColor = False
+        '
+        'cmdChiffre1
+        '
+        Me.cmdChiffre1.BackColor = System.Drawing.Color.Gold
+        Me.cmdChiffre1.Location = New System.Drawing.Point(42, 247)
+        Me.cmdChiffre1.Name = "cmdChiffre1"
+        Me.cmdChiffre1.Size = New System.Drawing.Size(31, 27)
+        Me.cmdChiffre1.TabIndex = 1
+        Me.cmdChiffre1.Text = "1"
+        Me.cmdChiffre1.UseVisualStyleBackColor = False
+        '
+        'cmdEffacer
+        '
+        Me.cmdEffacer.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.cmdEffacer.Location = New System.Drawing.Point(42, 328)
+        Me.cmdEffacer.Name = "cmdEffacer"
+        Me.cmdEffacer.Size = New System.Drawing.Size(31, 27)
+        Me.cmdEffacer.TabIndex = 3
+        Me.cmdEffacer.Text = "E"
+        Me.cmdEffacer.UseVisualStyleBackColor = False
+        '
+        'cmdChiffre8
+        '
+        Me.cmdChiffre8.BackColor = System.Drawing.Color.Gold
+        Me.cmdChiffre8.Location = New System.Drawing.Point(113, 147)
+        Me.cmdChiffre8.Name = "cmdChiffre8"
+        Me.cmdChiffre8.Size = New System.Drawing.Size(31, 27)
+        Me.cmdChiffre8.TabIndex = 8
+        Me.cmdChiffre8.Text = "8"
+        Me.cmdChiffre8.UseVisualStyleBackColor = False
+        '
+        'cmdChiffre5
+        '
+        Me.cmdChiffre5.BackColor = System.Drawing.Color.Gold
+        Me.cmdChiffre5.Location = New System.Drawing.Point(113, 197)
+        Me.cmdChiffre5.Name = "cmdChiffre5"
+        Me.cmdChiffre5.Size = New System.Drawing.Size(31, 27)
+        Me.cmdChiffre5.TabIndex = 5
+        Me.cmdChiffre5.Text = "5"
+        Me.cmdChiffre5.UseVisualStyleBackColor = False
+        '
+        'cmdChiffre2
+        '
+        Me.cmdChiffre2.BackColor = System.Drawing.Color.Gold
+        Me.cmdChiffre2.Location = New System.Drawing.Point(113, 247)
+        Me.cmdChiffre2.Name = "cmdChiffre2"
+        Me.cmdChiffre2.Size = New System.Drawing.Size(31, 27)
+        Me.cmdChiffre2.TabIndex = 2
+        Me.cmdChiffre2.Text = "2"
+        Me.cmdChiffre2.UseVisualStyleBackColor = False
+        '
+        'cmdChiffre0
+        '
+        Me.cmdChiffre0.BackColor = System.Drawing.Color.Gold
+        Me.cmdChiffre0.Location = New System.Drawing.Point(113, 294)
+        Me.cmdChiffre0.Name = "cmdChiffre0"
+        Me.cmdChiffre0.Size = New System.Drawing.Size(31, 27)
+        Me.cmdChiffre0.TabIndex = 0
+        Me.cmdChiffre0.Text = "0"
+        Me.cmdChiffre0.UseVisualStyleBackColor = False
+        '
+        'cmdChiffre9
+        '
+        Me.cmdChiffre9.BackColor = System.Drawing.Color.Gold
+        Me.cmdChiffre9.Location = New System.Drawing.Point(188, 147)
+        Me.cmdChiffre9.Name = "cmdChiffre9"
+        Me.cmdChiffre9.Size = New System.Drawing.Size(31, 27)
+        Me.cmdChiffre9.TabIndex = 9
+        Me.cmdChiffre9.Text = "9"
+        Me.cmdChiffre9.UseVisualStyleBackColor = False
+        '
+        'cmdChiffre6
+        '
+        Me.cmdChiffre6.BackColor = System.Drawing.Color.Gold
+        Me.cmdChiffre6.Location = New System.Drawing.Point(188, 197)
+        Me.cmdChiffre6.Name = "cmdChiffre6"
+        Me.cmdChiffre6.Size = New System.Drawing.Size(31, 27)
+        Me.cmdChiffre6.TabIndex = 6
+        Me.cmdChiffre6.Text = "6"
+        Me.cmdChiffre6.UseVisualStyleBackColor = False
+        '
+        'cmdChiffre3
+        '
+        Me.cmdChiffre3.BackColor = System.Drawing.Color.Gold
+        Me.cmdChiffre3.Location = New System.Drawing.Point(188, 247)
+        Me.cmdChiffre3.Name = "cmdChiffre3"
+        Me.cmdChiffre3.Size = New System.Drawing.Size(31, 27)
+        Me.cmdChiffre3.TabIndex = 3
+        Me.cmdChiffre3.Text = "3"
+        Me.cmdChiffre3.UseVisualStyleBackColor = False
+        '
+        'cmdDecimal
+        '
+        Me.cmdDecimal.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.cmdDecimal.Location = New System.Drawing.Point(188, 328)
+        Me.cmdDecimal.Name = "cmdDecimal"
+        Me.cmdDecimal.Size = New System.Drawing.Size(31, 27)
+        Me.cmdDecimal.TabIndex = 11
+        Me.cmdDecimal.Text = "."
+        Me.cmdDecimal.UseVisualStyleBackColor = False
+        '
+        'cmdDiviser
+        '
+        Me.cmdDiviser.BackColor = System.Drawing.Color.HotPink
+        Me.cmdDiviser.Location = New System.Drawing.Point(266, 147)
+        Me.cmdDiviser.Name = "cmdDiviser"
+        Me.cmdDiviser.Size = New System.Drawing.Size(31, 27)
+        Me.cmdDiviser.TabIndex = 12
+        Me.cmdDiviser.Text = "/"
+        Me.cmdDiviser.UseVisualStyleBackColor = False
+        '
+        'cmdPlus
+        '
+        Me.cmdPlus.BackColor = System.Drawing.Color.HotPink
+        Me.cmdPlus.Location = New System.Drawing.Point(327, 147)
+        Me.cmdPlus.Name = "cmdPlus"
+        Me.cmdPlus.Size = New System.Drawing.Size(31, 27)
+        Me.cmdPlus.TabIndex = 13
+        Me.cmdPlus.Text = "+"
+        Me.cmdPlus.UseVisualStyleBackColor = False
+        '
+        'cmdMultiplier
+        '
+        Me.cmdMultiplier.BackColor = System.Drawing.Color.HotPink
+        Me.cmdMultiplier.Location = New System.Drawing.Point(266, 294)
+        Me.cmdMultiplier.Name = "cmdMultiplier"
+        Me.cmdMultiplier.Size = New System.Drawing.Size(31, 27)
+        Me.cmdMultiplier.TabIndex = 14
+        Me.cmdMultiplier.Text = "*"
+        Me.cmdMultiplier.UseVisualStyleBackColor = False
+        '
+        'cmdMoins
+        '
+        Me.cmdMoins.BackColor = System.Drawing.Color.HotPink
+        Me.cmdMoins.Location = New System.Drawing.Point(327, 294)
+        Me.cmdMoins.Name = "cmdMoins"
+        Me.cmdMoins.Size = New System.Drawing.Size(31, 27)
+        Me.cmdMoins.TabIndex = 15
+        Me.cmdMoins.Text = "-"
+        Me.cmdMoins.UseVisualStyleBackColor = False
+        '
+        'cmdEgal
+        '
+        Me.cmdEgal.BackColor = System.Drawing.Color.OrangeRed
+        Me.cmdEgal.Location = New System.Drawing.Point(266, 197)
+        Me.cmdEgal.Name = "cmdEgal"
+        Me.cmdEgal.Size = New System.Drawing.Size(92, 77)
+        Me.cmdEgal.TabIndex = 16
+        Me.cmdEgal.Text = "="
+        Me.cmdEgal.UseVisualStyleBackColor = False
+        '
+        'lblAffichage
+        '
+        Me.lblAffichage.BackColor = System.Drawing.Color.LightPink
+        Me.lblAffichage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAffichage.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAffichage.ForeColor = System.Drawing.Color.White
+        Me.lblAffichage.Location = New System.Drawing.Point(42, 54)
+        Me.lblAffichage.Name = "lblAffichage"
+        Me.lblAffichage.Size = New System.Drawing.Size(316, 43)
+        Me.lblAffichage.TabIndex = 17
+        Me.lblAffichage.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Turquoise
+        Me.ClientSize = New System.Drawing.Size(388, 367)
+        Me.Controls.Add(Me.lblAffichage)
+        Me.Controls.Add(Me.cmdEgal)
+        Me.Controls.Add(Me.cmdMoins)
+        Me.Controls.Add(Me.cmdMultiplier)
+        Me.Controls.Add(Me.cmdPlus)
+        Me.Controls.Add(Me.cmdDiviser)
+        Me.Controls.Add(Me.cmdDecimal)
+        Me.Controls.Add(Me.cmdChiffre3)
+        Me.Controls.Add(Me.cmdChiffre6)
+        Me.Controls.Add(Me.cmdChiffre9)
+        Me.Controls.Add(Me.cmdChiffre0)
+        Me.Controls.Add(Me.cmdChiffre2)
+        Me.Controls.Add(Me.cmdChiffre5)
+        Me.Controls.Add(Me.cmdChiffre8)
+        Me.Controls.Add(Me.cmdEffacer)
+        Me.Controls.Add(Me.cmdChiffre1)
+        Me.Controls.Add(Me.cmdChiffre4)
+        Me.Controls.Add(Me.cmdChiffre7)
+        Me.Name = "form1"
+        Me.Text = "Calculatrice"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents cmdChiffre7 As System.Windows.Forms.Button
+    Friend WithEvents cmdChiffre4 As System.Windows.Forms.Button
+    Friend WithEvents cmdChiffre1 As System.Windows.Forms.Button
+    Friend WithEvents cmdEffacer As System.Windows.Forms.Button
+    Friend WithEvents cmdChiffre8 As System.Windows.Forms.Button
+    Friend WithEvents cmdChiffre5 As System.Windows.Forms.Button
+    Friend WithEvents cmdChiffre2 As System.Windows.Forms.Button
+    Friend WithEvents cmdChiffre0 As System.Windows.Forms.Button
+    Friend WithEvents cmdChiffre9 As System.Windows.Forms.Button
+    Friend WithEvents cmdChiffre6 As System.Windows.Forms.Button
+    Friend WithEvents cmdChiffre3 As System.Windows.Forms.Button
+    Friend WithEvents cmdDecimal As System.Windows.Forms.Button
+    Friend WithEvents cmdDiviser As System.Windows.Forms.Button
+    Friend WithEvents cmdPlus As System.Windows.Forms.Button
+    Friend WithEvents cmdMultiplier As System.Windows.Forms.Button
+    Friend WithEvents cmdMoins As System.Windows.Forms.Button
+    Friend WithEvents cmdEgal As System.Windows.Forms.Button
+    Friend WithEvents lblAffichage As System.Windows.Forms.Label
+
+End Class
---
+++ Calculatrice/Calculatrice/Form1.vb
@@ -0,0 +1,150 @@
+﻿Public Class form1
+
+    Dim Operand1 As Double
+    Dim Operand2 As Double
+    Dim Operator1 As String
+    Dim UtiliseEgal As Boolean
+
+    Private Sub cmdChiffre7_Click(sender As Object, e As EventArgs) Handles cmdChiffre7.Click
+        'Si la dernièr Opération est le signe égal on efface l'affichage
+        If UtiliseEgal Then
+            lblAffichage.Text = ""
+            UtiliseEgal = False
+        End If
+        lblAffichage.Text = lblAffichage.Text & cmdChiffre7.TabIndex
+    End Sub
+
+    Private Sub cmdChiffre0_Click(sender As Object, e As EventArgs) Handles cmdChiffre0.Click
+        'Si la dernièr Opération est le signe égal on efface l'affichage
+        If UtiliseEgal Then
+            lblAffichage.Text = ""
+            UtiliseEgal = False
+        End If
+        lblAffichage.Text = lblAffichage.Text & cmdChiffre0.TabIndex
+    End Sub
+
+
+    Private Sub cmdChiffre8_Click(sender As Object, e As EventArgs) Handles cmdChiffre8.Click
+        'Si la dernièr Opération est le signe égal on efface l'affichage
+        If UtiliseEgal Then
+            lblAffichage.Text = ""
+            UtiliseEgal = False
+        End If
+        lblAffichage.Text = lblAffichage.Text & cmdChiffre8.TabIndex
+    End Sub
+
+    Private Sub cmdChiffre1_Click(sender As Object, e As EventArgs) Handles cmdChiffre1.Click
+        'Si la dernièr Opération est le signe égal on efface l'affichage
+        If UtiliseEgal Then
+            lblAffichage.Text = ""
+            UtiliseEgal = False
+        End If
+        lblAffichage.Text = lblAffichage.Text & cmdChiffre1.TabIndex
+    End Sub
+    Private Sub cmdChiffre2_Click(sender As Object, e As EventArgs) Handles cmdChiffre2.Click
+        'Si la dernièr Opération est le signe égal on efface l'affichage
+        If UtiliseEgal Then
+            lblAffichage.Text = ""
+            UtiliseEgal = False
+        End If
+        lblAffichage.Text = lblAffichage.Text & cmdChiffre2.TabIndex
+    End Sub
+    Private Sub cmdChiffre3_Click(sender As Object, e As EventArgs) Handles cmdChiffre3.Click
+        'Si la dernièr Opération est le signe égal on efface l'affichage
+        If UtiliseEgal Then
+            lblAffichage.Text = ""
+            UtiliseEgal = False
+        End If
+        lblAffichage.Text = lblAffichage.Text & cmdChiffre3.TabIndex
+    End Sub
+    Private Sub cmdChiffre4_Click(sender As Object, e As EventArgs) Handles cmdChiffre4.Click
+        'Si la dernièr Opération est le signe égal on efface l'affichage
+        If UtiliseEgal Then
+            lblAffichage.Text = ""
+            UtiliseEgal = False
+        End If
+        lblAffichage.Text = lblAffichage.Text & cmdChiffre4.TabIndex
+    End Sub
+    Private Sub cmdChiffre5_Click(sender As Object, e As EventArgs) Handles cmdChiffre5.Click
+        'Si la dernièr Opération est le signe égal on efface l'affichage
+        If UtiliseEgal Then
+            lblAffichage.Text = ""
+            UtiliseEgal = False
+        End If
+        lblAffichage.Text = lblAffichage.Text & cmdChiffre5.TabIndex
+    End Sub
+    Private Sub cmdChiffre6_Click(sender As Object, e As EventArgs) Handles cmdChiffre6.Click
+        'Si la dernièr Opération est le signe égal on efface l'affichage
+        If UtiliseEgal Then
+            lblAffichage.Text = ""
+            UtiliseEgal = False
+        End If
+        lblAffichage.Text = lblAffichage.Text & cmdChiffre6.TabIndex
+    End Sub
+    Private Sub cmdChiffre9_Click(sender As Object, e As EventArgs) Handles cmdChiffre9.Click
+        'Si la dernièr Opération est le signe égal on efface l'affichage
+        If UtiliseEgal Then
+            lblAffichage.Text = ""
+            UtiliseEgal = False
+        End If
+        lblAffichage.Text = lblAffichage.Text & cmdChiffre9.TabIndex
+    End Sub
+
+
+    Private Sub cmdMultiplier_Click(sender As Object, e As EventArgs) Handles cmdMultiplier.Click
+        Operator1 = "*"
+        Operand1 = Val(lblAffichage.Text)
+        lblAffichage.Text = ""
+    End Sub
+
+    Private Sub cmdPlus_Click(sender As Object, e As EventArgs) Handles cmdPlus.Click
+        Operator1 = "+"
+        Operand1 = Val(lblAffichage.Text)
+        lblAffichage.Text = ""
+    End Sub
+
+    Private Sub cmdMoins_Click(sender As Object, e As EventArgs) Handles cmdMoins.Click
+        Operator1 = "-"
+        Operand1 = Val(lblAffichage.Text)
+        lblAffichage.Text = ""
+    End Sub
+
+    Private Sub cmdDecimal_Click(sender As Object, e As EventArgs) Handles cmdDecimal.Click
+        'Si la dernier opération est le signe égal on efface l'affichage
+        If UtiliseEgal Then
+            lblAffichage.Text = ""
+            UtiliseEgal = False
+        End If
+
+        lblAffichage.Text = lblAffichage.Text + "."
+    End Sub
+
+    Private Sub cmdEffacer_Click(sender As Object, e As EventArgs) Handles cmdEffacer.Click
+        lblAffichage.Text = ""
+    End Sub
+    Private Sub cmdEgal_Click(sender As Object, e As EventArgs) Handles cmdEgal.Click
+        Dim Result As Double
+
+        Operand2 = Val(lblAffichage.Text)
+        If Operator1 = "+" Then
+            Result = Operand1 + Operand2
+        ElseIf Operator1 = "-" Then
+            Result = Operand1 - Operand2
+        ElseIf Operator1 = "*" Then
+            Result = Operand1 * Operand2
+        ElseIf Operator1 = "/" Then
+            If Operand2 <> 0 Then
+                Result = Operand1 / Operand2
+
+            End If
+        End If
+        lblAffichage.Text = Result
+        'Laisser Savoir qu'on vient juste d'utiliser le signe égal
+        UtiliseEgal = True
+    End Sub
+    Private Sub cmdDiviser_Click(sender As Object, e As EventArgs) Handles cmdDiviser.Click
+        Operator1 = "/"
+        Operand1 = Val(lblAffichage.Text)
+        lblAffichage.Text = ""
+    End Sub
+End Class
